0de4bf04bd28863714e6dcb62ef685437fa856af
AymaneLaghrieb <aymane.laghrieb.1@ens.etsmtl.ca>
2025-03-31 13:14:53 -0400

    La recherche de quiz par texte est implémentée dans le front-end

--- search.js
+++ search.js
@@ -1,4 +1,18 @@
 let quizzes = [];
+const isEmpty = str => !str.trim().length;
+
+function selectQuizDisplay() {
+    if (window.location.pathname.includes("search.html")) {
+        document.getElementById("search-btn").addEventListener("click", function() {
+            let searchInput = document.getElementById("search-input").value;
+            if (isEmpty(searchInput)) {
+                fetchQuizzes();
+            } else {
+                fetchQuizzesBySearch();
+            }
+        });
+    }
+}
 
 function fetchQuizzes() {
     if (window.location.pathname.includes("search.html")) {
@@ -18,7 +32,7 @@ function fetchQuizzes() {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    fetchQuizzes();
+    selectQuizDisplay();
 });
 
 
@@ -58,7 +72,52 @@ function displayQuizzes() {
 
 function fetchQuizzesBySearch() {
     if (window.location.pathname.includes("search.html")) {
-        
-
+        let recherche = document.getElementById("search-input").value;
+        fetch(`http://localhost:8000/quizzes/find?search=${recherche}`)
+            .then(response => {
+                if(!response.ok) {
+                    throw new Error('Erreur lors de la récupération des quiz');
+                }
+                return response.json();
+            })
+            .then(data => {
+                quizzes = data;
+                displayQuizzesBySearch();
+            })
+            .catch(error => {
+                console.error("Erreur:", error);
+                document.getElementById('quiz-results').innerHTML = "<p>Aucun quiz n'a été trouvé</p>";
+            });
     }
+}
+
+function displayQuizzesBySearch() {
+    const quizContainer = document.getElementById('quiz-results');
+    quizContainer.innerHTML = "";
+
+    if (!quizzes || quizzes.length === 0) {
+        quizContainer.innerHTML = "<p>Aucun quiz trouvé.</p>";
+        return;
+    }
+
+    quizzes.forEach(quiz => {
+        const quizElement = document.createElement("div");
+        quizElement.classList.add("quiz-results");
+
+        quizElement.innerHTML = `
+            <div id="quiz-result-card-${quiz.id_quiz}" class="quiz-result-card">
+                <div class="result-details">
+                    <h4>${quiz.nom}</h4>
+                    <p>${quiz.description}</p>
+                    <div class="result-meta">
+                        <span class="result-category">${quiz.categorie}</span>
+                        <span class="result-author">Par ${quiz.nomCreateur}</span>
+                    </div>
+                </div>
+                <a href="#" class="play-btn">Jouer</a>
+            </div>
+        `;
+
+        quizContainer.appendChild(quizElement);
+    });
 }
