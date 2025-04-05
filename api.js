f36874953a4c7c3e4c07be6a80b49b7ef66fd5ca
AymaneLaghrieb <aymane.laghrieb.1@ens.etsmtl.ca>
2025-04-05 13:17:35 -0400

    Optimisation de la recherche de quiz

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 // api.js
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'http://localhost:8000/api';
 
 // Fonction pour récupérer tous les quiz
 async function getAllQuizzes() {
--- search.js
+++ search.js
@@ -1,13 +1,31 @@
 let quizzes = [];
+let count = [];
+let searchInput = "";
 const isEmpty = str => !str.trim().length;
 
 function selectQuizDisplay() {
     if (window.location.pathname.includes("search.html")) {
         fetchQuizzes();
+        document.getElementById("search-input").addEventListener('keydown', function(e) {
+            if(e.key === 'Enter') {
+            searchInput = document.getElementById("search-input").value;
+            console.log(searchInput);
+            if (isEmpty(searchInput)) {
+                fetchQuizzes();
+            } else if (searchInput === "random" || searchInput === "hasard"){
+                fetchRandomQuiz();
+            } else {
+                fetchQuizzesBySearch();
+            }
+        }
+        });
+
         document.getElementById("search-btn").addEventListener("click", function() {
             let searchInput = document.getElementById("search-input").value;
             if (isEmpty(searchInput)) {
                 fetchQuizzes();
+            } else if (searchInput === "random" || searchInput === "hasard"){
+                fetchRandomQuiz();
             } else {
                 fetchQuizzesBySearch();
             }
@@ -16,20 +34,23 @@ function selectQuizDisplay() {
 }
 
 function fetchQuizzes() {
-    if (window.location.pathname.includes("search.html")) {
 
-        fetch('http://localhost:8000/quizzes')
-            .then(response => {
-                if(!response.ok) {
-                    throw new Error('Erreur lors de la récupération des quiz');
-                }
-                return response.json();
-            })
-            .then(data => {
-                quizzes = data;
-                displayQuizzes();
-            })
-    }
+    fetch('http://localhost:8000/api/quizzes')
+        .then(response => response.json())
+        .then(data => {
+            if (data.count === 0) {
+                displayNoQuizMessage(data.message || 'Aucun quiz trouvé');
+            } else {
+                quizzes = data.quizzes;
+                count = data.count;
+                displayQuizzes();    
+            }
+        })
+        .catch(error => {
+            console.error('Erreur réseau ou serveur: ', error);
+            displayErrorMessage('Une erreur est survenue lors de la récupération des quiz.');
+        });
+    
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -63,62 +84,60 @@ function displayQuizzes() {
                 <a href="#" class="play-btn" i="play-btn-${quiz.id_quiz}">Jouer</a>
             </div>
         `;
-        console.log("Test");
-        console.log(quizContainer);
-
         quizContainer.appendChild(quizElement);
     })
+    document.getElementById("results-number").innerHTML = count + " resultats trouvés";
+    document.getElementById("search-term").innerHTML = searchInput;
 
 }
 
 function fetchQuizzesBySearch() {
-    if (window.location.pathname.includes("search.html")) {
-        let recherche = document.getElementById("search-input").value;
-        fetch(`http://localhost:8000/quizzes/find?search=${recherche}`)
-            .then(response => {
-                if(!response.ok) {
-                    throw new Error('Erreur lors de la récupération des quiz');
-                }
-                return response.json();
-            })
-            .then(data => {
-                quizzes = data;
-                displayQuizzesBySearch();
-            })
-            .catch(error => {
-                console.error("Erreur:", error);
-                document.getElementById('quiz-results').innerHTML = "<p>Aucun quiz n'a été trouvé</p>";
-            });
-    }
+    let recherche = document.getElementById("search-input").value;
+    fetch(`http://localhost:8000/api/quizzes/find?search=${recherche}`)
+        .then(response => {
+            if(!response.ok) {
+                throw new Error('Erreur lors de la récupération des quiz');
+            }
+            return response.json();
+        })
+        .then(data => {
+            quizzes = data.quizzes;
+            count = data.count;
+            displayQuizzes();
+        })
+        .catch(error => {
+            console.error("Erreur:", error);
+            document.getElementById('quiz-results').innerHTML = "<p>Aucun quiz n'a été trouvé</p>";
+        });
 }
 
-function displayQuizzesBySearch() {
-    const quizContainer = document.getElementById('quiz-results');
-    quizContainer.innerHTML = "";
-
-    if (!quizzes || quizzes.length === 0) {
-        quizContainer.innerHTML = "<p>Aucun quiz trouvé.</p>";
-        return;
-    }
-
-    quizzes.forEach(quiz => {
-        const quizElement = document.createElement("div");
-        quizElement.classList.add("quiz-results");
-
-        quizElement.innerHTML = `
-            <div id="quiz-result-card-${quiz.id_quiz}" class="quiz-result-card">
-                <div class="result-details">
-                    <h4>${quiz.nom}</h4>
-                    <p>${quiz.description}</p>
-                    <div class="result-meta">
-                        <span class="result-category">${quiz.categorie}</span>
-                        <span class="result-author">Par ${quiz.nomCreateur}</span>
-                    </div>
-                </div>
-                <a href="#" class="play-btn">Jouer</a>
-            </div>
-        `;
-
-        quizContainer.appendChild(quizElement);
+function fetchRandomQuiz() {
+    fetch('http://localhost:8000/api/quizzes/random')
+    .then(response => {
+        if(!response.ok) {
+            throw new Error('Erreur lors de la récupération des quiz');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log("Liste des quiz", data)
+        quizzes = [data.quiz];
+        count = data.count;
+        displayQuizzes();
+    })
+    .catch(error => {
+        console.error("Erreur:", error);
+        document.getElementById('quiz-results').innerHTML = "<p>Aucun quiz n'a été trouvé</p>";
     });
+
+}
+
+function displayNoQuizMessage(message) {
+    const container = document.getElementById('quiz-container');
+    container.innerHTML = `<p>${message}</p>`;
+}
+
+function displayErrorMessage(message) {
+    const container = document.getElementById('quiz-container');
+    container.innerHTML = `<p style="color: red;">${message}</p>`;
 }
