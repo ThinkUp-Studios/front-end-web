c22dab610b502ee87a0de7c6c75d12110b9ae33d
RaphaelMC27 <CRMAC5@ulaval.ca>
2025-04-06 20:52:41 -0400

    not good

---
+++ login.js
@@ -0,0 +1,34 @@
+async function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        // envoie login information au back-end
+        const response = await fetch('http://localhost:8000/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            // Enregistre donnees utilisateur dans localStorage 
+            localStorage.setItem('user', JSON.stringify(data.utilisateur)); 
+            window.location.href = 'main.html'; 
+        } else {
+            alert(data.error || 'Identifiants invalides');
+        }
+    } catch (error) {
+        console.error('Login error:', error);
+        alert('Une erreur est survenue. Veuillez réessayer.');
+    }
+}
+
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();  
+    login();  
+});
+
